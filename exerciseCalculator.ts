c941683465ba64b6bc357ee7aa467740f2127d92
serhatprogramming <serhatprogramming@gmail.com>
2023-05-09 16:40:48 -0400

    calculateexercises giving the right values.

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,5 +1,40 @@
-const calculateExercises = (exercises: number[]): string => {
-  return `The number of exercised days is ${exercises.length}`;
+interface ExerciseStats {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (
+  exercises: number[],
+  target: number
+): ExerciseStats => {
+  const trainingDays = exercises.filter((number) => number !== 0).length;
+  const totalHours = exercises.reduce((a, b) => a + b);
+
+  let rating = 2;
+  let ratingDescription = "just reached your target, keep up the good work!";
+
+  if (target > totalHours / exercises.length) {
+    rating = 1;
+    ratingDescription = "not too bad but could be better";
+  } else if (target < totalHours / exercises.length) {
+    rating = 3;
+    ratingDescription = "you rock!";
+  }
+
+  return {
+    periodLength: exercises.length,
+    trainingDays,
+    success: target <= totalHours / exercises.length,
+    rating,
+    ratingDescription,
+    target,
+    average: totalHours / exercises.length,
+  };
 };
 
-console.log(calculateExercises([123, 123, 123, 1231]));
+console.log(calculateExercises([4, 1, 2, 6, 0], 2));
